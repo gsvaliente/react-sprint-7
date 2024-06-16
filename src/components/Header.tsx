c74bab2a05d6dea668c6081496509487c7af111d
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-06-16 17:15:55 +0200

    Added Tailwind

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header className="flex">
+      <h1 className="text-3xl font-bold underline">LOGO</h1>
+      <aside>
+        <p>Sign In</p>
+        <p>Register</p>
+      </aside>
+    </header>
+  );
+}
