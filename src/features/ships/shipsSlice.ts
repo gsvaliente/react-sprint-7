c3e2629ec51d5a91a7597426e755aa95986fdb47
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-07-02 07:52:52 +0200

    Added a thunk to get single ship by id

--- src/features/ships/shipsSlice.ts
+++ src/features/ships/shipsSlice.ts
@@ -5,12 +5,14 @@ import type { StarshipType } from '../../types/Ship.interface';
 
 export interface ShipsState {
   shipList: StarshipType[];
+  ship: StarshipType | undefined;
   isLoading: boolean;
   isError: string;
 }
 
 const initialState: ShipsState = {
   shipList: [],
+  ship: undefined,
   isLoading: false,
   isError: '',
 };
@@ -23,6 +25,11 @@ const shipsSlice = createSlice({
       state.isLoading = false;
       state.isError = '';
       state.shipList = action.payload;
+    },
+    findShip(state, action: PayloadAction<StarshipType>) {
+      state.isLoading = false;
+      state.isError = '';
+      state.ship = action.payload;
     },
     notFound(state, action: PayloadAction<string>) {
       state.isError = action.payload;
@@ -38,6 +45,7 @@ export function findShips(url: string) {
   if (!url)
     return {
       type: 'ships/notFound',
+      payload: 'No Url Provided',
     };
 
   return async function (dispatch: AppDispatch) {
@@ -48,6 +56,26 @@ export function findShips(url: string) {
       const data = await res.json();
 
       dispatch({ type: 'ships/findShips', payload: data.results });
+    } catch (error: any) {
+      dispatch({ type: 'ships/notFound', payload: error.message });
+    }
+  };
+}
+
+export function findShip(url: string) {
+  if (!url)
+    return {
+      type: 'ships/notFound',
+      payload: 'No Url Provided',
+    };
+
+  return async function (dispatch: AppDispatch) {
+    try {
+      dispatch({ type: 'ships/findingShips' });
+      const res = await fetch(url);
+      const data = await res.json();
+
+      dispatch({ type: 'ships/findShip', payload: data });
     } catch (error: any) {
       dispatch({ type: 'ships/notFound', payload: error.message });
     }
